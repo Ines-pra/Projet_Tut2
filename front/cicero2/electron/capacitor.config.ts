73698d02de0853df91ad973156969fbb9edb1f5f
GNK-0DAY UZUMAKI <gsfproject2018@gmail.com>
2022-03-16 11:02:14 +0100

    création du squelette de l'application

---
+++ front/cicero2/electron/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.cicero2.app',
+  appName: 'cicero2',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
